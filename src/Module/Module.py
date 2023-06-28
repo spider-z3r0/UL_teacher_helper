a98a965cfcf9df8c215a5ad3fd6abc9de0a6243c
Kevin.OMalley <Kevin.OMalley@ul.ie>
2023-06-28 15:14:10 +0100

    changed some of the import statments

--- src/Module/Module.py
+++ src/Module/Module.py
@@ -1,4 +1,4 @@
-from .dependencies import *
+from .dependencies import pl, logging, datetime
 
 
 class Module:
--- src/Module/dependencies.py
+++ src/Module/dependencies.py
@@ -1,6 +1,6 @@
-import pathlib as pl
 import pandas as pd
 import logging
 from datetime import datetime
 from typing import Union, List, Dict, Tuple, Optional
 import xlwings as xw
+import pathlib as pl
